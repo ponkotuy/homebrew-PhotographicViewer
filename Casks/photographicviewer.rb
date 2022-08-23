a4b6523331c288d296ea48782c7ac0e43bbf8e06
b-wind <perl.mania@gmail.com>
2022-08-24 02:07:28 +0900

    first commit.

---
+++ Casks/photographicviewer.rb
@@ -0,0 +1,10 @@
+cask "photographicviewer" do
+    version "0.3.0"
+    sha256 "6716db8daabf39c69e9722bd7bc74043bec05e71c29fbc01ea950c9fa4a53f5c"
+  
+    url "https://github.com/ponkotuy/PhotographicViewer/releases/download/v0.3.0/PhotographicViewer.mac.zip"
+    name "PhotographicViewer"
+    homepage "https://developer.1password.com/docs/cli"
+
+    app "PhotographicViewer.app"
+end
