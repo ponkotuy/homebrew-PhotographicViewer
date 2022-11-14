0eeb1ae36cb490eb7020685534c5f41b57816fdb
ponkotuy <web@ponkotuy.com>
2022-11-14 21:57:48 +0900

    Update 0.6.0

--- Casks/photographicviewer.rb
+++ Casks/photographicviewer.rb
@@ -1,8 +1,8 @@
 cask "photographicviewer" do
-    version "0.5.0"
-    sha256 "cbfa76ea1186e88c5d9b245b93f47d176b49a30ca70bbabcc1e8315d5c6bcb22"
+    version "0.6.0"
+    sha256 "c3285cbac47ced64c4c5c966ae4b150630633afd99ead06968f208c743768152"
 
-    url "https://github.com/ponkotuy/PhotographicViewer/releases/download/v0.5.0/PhotographicViewer.mac.zip"
+    url "https://github.com/ponkotuy/PhotographicViewer/releases/download/v0.6.0/PhotographicViewer.mac.zip"
     name "PhotographicViewer"
     homepage "https://github.com/ponkotuy/PhotographicViewer"
 
